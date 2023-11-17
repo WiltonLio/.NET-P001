646cf305fe57b5a01d2312c9100b41d8161ae30f
Wilton Freire <wilton.lio@gmail.com>
2023-11-17 19:54:25 -0300

    Resposta 1 e 2

--- PO001/Program.cs
+++ PO001/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2.Tipos de Dados:
+// Problema: Quais são os tipos de dados numéricos inteiros disponíveis no .NET? Dê exemplos de uso para cada um deles
+// através de exemplos.
+
+#region Tipos de Dados
+
+        int TipoInt = 42;
+        Console.WriteLine($"Tipo int: {TipoInt}");
+        long TipoLong = 1234567890123456;
+        Console.WriteLine($"Tipo long: {TipoLong}");
+        short TipoShort = 32767;
+        Console.WriteLine($"Tipo short: {TipoShort}");
+        byte TipoByte = 255;
+        Console.WriteLine($"Tipo byte: {TipoByte}");
+        sbyte TipoSByte = -128;
+        Console.WriteLine($"Tipo sbyte: {TipoSByte}");
+        ushort TipoUShort = 65535;
+        Console.WriteLine($"Tipo ushort: {TipoUShort}");
+        uint TipoUInt = 4294967295;
+        Console.WriteLine($"Tipo uint: {TipoUInt}");
+        ulong TipoULong = 18446744073709551615;
+        Console.WriteLine($"Tipo ulong: {TipoULong}");
+        
+#endregion
+
+// 3.Conversão de Tipos de Dados:
+// Problema: Suponha que você tenha uma variável do tipo double e deseja convertê-la em um tipo int. Como você faria essa
+// conversão e o que aconteceria se a parte fracionária da variável double não pudesse ser convertida em um int? Resolva o 
+// problema através de um exemplo em C#.
+
+// 4.Operadores Aritméticos:
+// Problema: Dada a variável int x = 10 e a variável int y = 3, escreva código para calcular e exibir o resultado da adição,
+// subtração, multiplicação e divisão de x por y.
+
+// 5.Operadores de Comparação:
+// Problema: Considere as variáveis int a = 5 e int b = 8. Escreva código para determinar se a é maior que b e exiba o resultado.
+
+// 6.Operadores de Igualdade:
+// Problema: Você tem duas strings, string str1 = "Hello" e string str2 = "World". Escreva código para verificar se as duas
+// strings são iguais e exibir o resultado.
+
+// 7.Operadores Lógicos:
+// Problema: Suponha que você tenha duas variáveis booleanas, bool condicao1 = true e bool condicao2 = false. Escreva código
+// para verificar se ambas as condições são verdadeiras e exiba o resultado.
+
+// 8.Desafio de Mistura de Operadores:
+// Problema: Dadas as variáveis int num1 = 7, int num2 = 3, e int num3 = 10, escreva código para verificar se num1 é maior do
+// que num2 e se num3 é igual a num1 + num2.
---
+++ Readme.md
@@ -0,0 +1,13 @@
+1.Configuração do Ambiente:
+
+Problema: Como você pode verificar se o .NET SDK está corretamente instalado em seu sistema? Em um arquivo de texto ou Markdown, liste os comandos que podem ser usados para verificar a(s) versão(ões) do .NET SDK instalada(s), como remover e atualizar.
+
+Comando: dotnet --version (Exibir a versão do .NET)
+
+Comando: dotnet-core-uninstall list (Listar todos os SDKs do .NET que podem ser removidos com essa ferramenta)
+
+Comando: dotnet-core-uninstall list --x64 --x86 (Listar todos os SDKs do .NET que podem ser removidos com essa ferramenta)
+
+Comando: dotnet-core-uninstall list --all (Listar todos os SDKs do .NET que podem ser removidos com essa ferramenta)
+
+Comando: dotnet-core-update list (Listar todos os SDKs do .NET que podem ser atualizados com essa ferramenta)
